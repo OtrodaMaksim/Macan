20718312ef67d571c3b80b7f1f015faf65ec6ea6
OtrodaMaksim <59541372+OtrodaMaksim@users.noreply.github.com>
2020-02-12 20:51:32 +0200

    git add

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function() {
+	// slider
+	$('.your_class').slick({
+		slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+	});
+	// background_slider
+	$('.background_slider').slick({
+		slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+	});
+// slider_citation
+$('.slider_citation').slick({
+	slidesToShow: 1,
+slidesToScroll: 1,
+autoplay: true,
+autoplaySpeed: 5000,
+cssEase:'linear',
+});
+
+});
